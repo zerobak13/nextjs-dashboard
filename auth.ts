a7cbf43c1974aa7c344869af37b1dc605a341060
zerobak13 <pjy0807111@naver.com>
2026-01-21 13:59:46 +0900

    Add NextAuth API route

--- auth.ts
+++ auth.ts
@@ -18,7 +18,7 @@ async function getUser(email: string): Promise<User | undefined> {
   }
 }
  
-export const { auth, signIn, signOut } = NextAuth({
+export const {handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
